00d54f8370ba8f5a33b21a89dac642829184de1b
Laura Silvanaviciute <laura.silvanaviciute@shopify.com>
2022-02-20 21:22:41 +0200

    refactor: add each instrument as separate comp

--- src/features/instrument/Instrument.tsx
+++ src/features/instrument/Instrument.tsx
@@ -2,7 +2,7 @@ import { Button } from '@mui/material';
 import './Instrument.css';
 import { Synth, Volume, Sampler, Destination, PolySynth, Transport, Part, Noise, AutoFilter, Player } from 'tone'
 import { BASS_CHORDS, GAMMA_NOTES, MARIO_SAMPLES, MELODY_CHORDS } from '../../constants/notes'
-import { useRef } from 'react';
+import { useRef, MutableRefObject } from 'react';
 const DogSample = require("../../assets/dog-sample.mp3");
 
 const keyStyles = {
@@ -21,17 +21,10 @@ const polySynthSquare = new PolySynth(Synth, {
 }).chain(new Volume(-25), Destination);
 
 export const Instrument = () => {
-  // @TODO: separate to piaoa nd predefined isntruments
-
   const isTransportStarted = useRef(false);
   const melodyPart = useRef<Part | null>(null);
   const bassPart = useRef<Part | null>(null);
   const noise = useRef<Noise | null>(null);
-
-  function playNote(note: string) {
-    polySynthSquare.triggerAttackRelease(note, "8n");
-    sampler.triggerAttack(note);
-  }
 
   function playPart(part: Part) {
     if (!isTransportStarted.current) {
@@ -45,7 +38,52 @@ export const Instrument = () => {
     part.loopEnd = "4m";
   }
 
-  // @TODO: maybe move to separate components, maybe same file
+  // @TODO: check arent a beter way to write each thing now (always create new player?), also check updates in tone
+  function playSample(sampleName: string) {
+    new Player({
+      url: sampleName,
+      autostart: true,
+    }).chain(mediumVolume, Destination);
+  }
+
+  return (
+    <div className="Instrument">
+      <Keys></Keys>
+      <div className="PredefinedMusic">
+        <Melody melodyPart={melodyPart} playPart={playPart}></Melody>
+        <Bass bassPart={bassPart} playPart={playPart}></Bass>
+        <NoiseSound noise={noise}></NoiseSound>
+        <MarioSamples playSample={playSample}></MarioSamples>
+        <SpeedUpBpm></SpeedUpBpm>
+        <StopInstruments playSample={playSample} melodyPart={melodyPart} bassPart={bassPart} noise={noise}></StopInstruments>
+      </div>
+    </div>
+
+  );
+}
+
+export const Keys = () => {
+  function playNote(note: string) {
+    polySynthSquare.triggerAttackRelease(note, "8n");
+    sampler.triggerAttack(note);
+  }
+
+  return (
+    <div>
+      {GAMMA_NOTES.map(note => (
+        <Button onClick={() => playNote(note)} key={note} sx={keyStyles}>♪ {note}</Button>
+      ))}
+    </div>
+  );
+}
+
+//hmm slowly becomes a monster
+interface MelodyProps {
+  melodyPart: MutableRefObject<Part | null>,
+  playPart: (part: Part) => void,
+};
+
+export const Melody = ({ melodyPart, playPart }: MelodyProps) => {
   function toggleMelody() {
     if (melodyPart.current) {
       melodyPart.current.mute = !melodyPart.current.mute;
@@ -73,6 +111,19 @@ export const Instrument = () => {
     }
   }
 
+  return (
+    <>
+      <Button onClick={toggleMelody}>🎹 Melody</Button>
+    </>
+  );
+}
+
+interface BassProps {
+  bassPart: MutableRefObject<Part | null>,
+  playPart: (part: Part) => void,
+};
+
+export const Bass = ({ bassPart, playPart }: BassProps) => {
   function toggleBass() {
     if (bassPart.current) {
       bassPart.current.mute = !bassPart.current.mute;
@@ -88,6 +139,18 @@ export const Instrument = () => {
     }
   }
 
+  return (
+    <>
+      <Button onClick={toggleBass}>🎸 Bass</Button>
+    </>
+  );
+}
+
+interface NoiseSoundProps {
+  noise: MutableRefObject<Noise | null>,
+};
+
+export const NoiseSound = ({ noise }: NoiseSoundProps) => {
   function toggleNoise() {
     if (noise.current) {
       noise.current.mute = !noise.current.mute;
@@ -101,24 +164,51 @@ export const Instrument = () => {
     }
   }
 
-  // @TODO: check arent a beter way to write each thing now (always create new player?), also check updates in tone
-  function playSample(sampleName: string) {
-    new Player({
-      url: sampleName,
-      autostart: true,
-    }).chain(mediumVolume, Destination);
-  }
+  return (
+    <>
+      <Button onClick={toggleNoise}>💨 Noise</Button>
+    </>
+  );
+}
 
+// rethink is interface is needed (maybe due to consistence + pretty long one)
+interface MarioSamplesProps {
+  playSample: (sample: string) => void,
+};
+
+export const MarioSamples = ({ playSample }: MarioSamplesProps) => {
   function playRandomMarioSample() {
     playSample(
       MARIO_SAMPLES[Math.floor(Math.random() * MARIO_SAMPLES.length)]
     );
   }
 
+  return (
+    <>
+      <Button onClick={playRandomMarioSample}>🍄 Mario</Button>
+    </>
+  );
+}
+
+export const SpeedUpBpm = () => {
   function speedUpBpm() {
     Transport.bpm.value += 20;
   }
 
+  return (
+    <>
+      <Button onClick={speedUpBpm}>👟 Speed up</Button>
+    </>
+  );
+}
+
+interface StopInsrumentsProps {
+  playSample: (sample: string) => void,
+  melodyPart: MutableRefObject<Part | null>,
+  bassPart: MutableRefObject<Part | null>,
+  noise: MutableRefObject<Noise | null>,
+};
+export const StopInstruments = ({ playSample, melodyPart, bassPart, noise }: StopInsrumentsProps) => {
   function stopInstrument(instrument: Part | Noise | null) {
     if (instrument) {
       instrument.mute = true;
@@ -135,21 +225,8 @@ export const Instrument = () => {
   }
 
   return (
-    <div className="Instrument">
-      <div>
-        {GAMMA_NOTES.map(note => (
-          <Button onClick={() => playNote(note)} key={note} sx={keyStyles}>♪ {note}</Button>
-        ))}
-      </div>
-      <div className="PredefinedMusic">
-        <Button onClick={toggleMelody}>🎹 Melody</Button>
-        <Button onClick={toggleBass}>🎸 Bass</Button>
-        <Button onClick={toggleNoise}>💨 Noise</Button>
-        <Button onClick={playRandomMarioSample}>🍄 Mario</Button>
-        <Button onClick={speedUpBpm}>👟 Speed up</Button>
-        <Button onClick={stopInstruments}>🖐 Stop</Button>
-      </div>
-    </div>
-
+    <>
+      <Button onClick={stopInstruments}>🖐 Stop</Button>
+    </>
   );
 }
