6257822b9b7ea4d2f1c4944820f1008b87933593
Laura Silvanaviciute <laura.silvanaviciute@shopify.com>
2022-02-20 20:31:54 +0200

    refactor: eliminate useState, move static values outside componnt

--- src/features/instrument/Instrument.tsx
+++ src/features/instrument/Instrument.tsx
@@ -2,7 +2,7 @@ import { Button } from '@mui/material';
 import './Instrument.css';
 import { Synth, Volume, Sampler, Destination, PolySynth, Transport, Part, Noise, AutoFilter, Player } from 'tone'
 import { BASS_CHORDS, GAMMA_NOTES, MARIO_SAMPLES, MELODY_CHORDS } from '../../constants/notes'
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 const DogSample = require("../../assets/dog-sample.mp3");
 
 const keyStyles = {
@@ -10,36 +10,33 @@ const keyStyles = {
   paddingBottom: '4em',
   width: '5em'
 };
+const mediumVolume = new Volume(-15);
+const sampler = new Sampler({
+  E4: DogSample,
+}).chain(mediumVolume, Destination);
+const polySynthSquare = new PolySynth(Synth, {
+  oscillator: {
+    type: "square",
+  },
+}).chain(new Volume(-25), Destination);
 
 export const Instrument = () => {
-  console.log('render')
   // @TODO: separate to piaoa nd predefined isntruments
 
-  const [isTransportStarted, setIsTransportStarted] = useState(false);
-  const [melodyPart, setMelodyPart] = useState<Part | null>(null);
-  const [bassPart, setBassPart] = useState<Part | null>(null);
-  const [noise, setNoise] = useState<Noise | null>(null);
-
-  const mediumVolume = useRef(new Volume(-15)).current;
-  const sampler = useRef(new Sampler({
-    E4: DogSample,
-  }).chain(mediumVolume, Destination)).current;
-  const polySynthSquare = useRef(new PolySynth(Synth, {
-    oscillator: {
-      type: "square",
-    },
-  }).chain(new Volume(-25), Destination)).current;
+  const isTransportStarted = useRef(false);
+  const melodyPart = useRef<Part | null>(null);
+  const bassPart = useRef<Part | null>(null);
+  const noise = useRef<Noise | null>(null);
 
   function playNote(note: string) {
     polySynthSquare.triggerAttackRelease(note, "8n");
     sampler.triggerAttack(note);
   }
 
-  // @TODO: add types
   function playPart(part: Part) {
-    if (!isTransportStarted) {
+    if (!isTransportStarted.current) {
       Transport.toggle();
-      setIsTransportStarted(true)
+      isTransportStarted.current = true;
       Transport.bpm.value = 132;
     }
 
@@ -50,8 +47,8 @@ export const Instrument = () => {
 
   // @TODO: maybe move to separate components, maybe same file
   function toggleMelody() {
-    if (melodyPart) {
-      melodyPart.mute = !melodyPart.mute;
+    if (melodyPart.current) {
+      melodyPart.current.mute = !melodyPart.current.mute;
     } else {
       const polySynthSaw = new PolySynth(Synth, {
         oscillator: {
@@ -62,7 +59,8 @@ export const Instrument = () => {
           release: 0.4,
         },
       }).chain(mediumVolume, Destination);
-      const melody = new Part((time, chord) => {
+
+      melodyPart.current = new Part((time, chord) => {
         sampler.triggerAttackRelease(chord.note, chord.duration, time);
         polySynthSaw.triggerAttackRelease(
           chord.note,
@@ -71,46 +69,39 @@ export const Instrument = () => {
         );
       }, MELODY_CHORDS);
 
-      // maybe can be used only hook value and setting without extra const
-      playPart(melody);
-      setMelodyPart(melody);
+      playPart(melodyPart.current);
     }
   }
 
   function toggleBass() {
-    if (bassPart) {
-      bassPart.mute = !bassPart.mute;
+    if (bassPart.current) {
+      bassPart.current.mute = !bassPart.current.mute;
     } else {
-      const bass = new Part((time, chord) => {
+      bassPart.current = new Part((time, chord) => {
         polySynthSquare.triggerAttackRelease(
           chord.note,
           chord.duration,
           time
         );
       }, BASS_CHORDS);
-
-      playPart(bass);
-      setBassPart(bass)
+      playPart(bassPart.current);
     }
   }
 
   function toggleNoise() {
-    if (noise) {
-      noise.mute = !noise.mute;
+    if (noise.current) {
+      noise.current.mute = !noise.current.mute;
     } else {
-      const noiseSounds = new Noise("pink").start();
+      noise.current = new Noise("pink").start();
       const filter = new AutoFilter({
         frequency: "8m",
       }).chain(new Volume(-20), Destination);
-
-      setNoise(noiseSounds)
-
-      noiseSounds.connect(filter);
+      noise.current.connect(filter);
       filter.start();
     }
   }
 
-  // @TODO: review isnt a beter way to write each thing now (always create new player?), also any diff in react
+  // @TODO: check arent a beter way to write each thing now (always create new player?), also check updates in tone
   function playSample(sampleName: string) {
     new Player({
       url: sampleName,
@@ -132,16 +123,15 @@ export const Instrument = () => {
     if (instrument) {
       instrument.mute = true;
       instrument.stop();
-      // instrument = null;
     }
   }
 
   function stopInstruments() {
     playSample(MARIO_SAMPLES[5]);
 
-    stopInstrument(melodyPart);
-    stopInstrument(bassPart);
-    stopInstrument(noise);
+    stopInstrument(melodyPart.current);
+    stopInstrument(bassPart.current);
+    stopInstrument(noise.current);
   }
 
   return (
