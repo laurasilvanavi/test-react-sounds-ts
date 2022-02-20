6f6436c54d24fd69dd4ef53cbb776845dfb1db66
Laura Silvanaviciute <laura.silvanaviciute@shopify.com>
2022-02-20 14:45:52 +0200

    feat: add polysynth, transport, part, noise, player instruments using predefined notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css';
 import { Instrument } from './features/instrument';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import teal from '@mui/material/colors/teal';
-import commonColors from '@mui/material/colors/common';
 import { AppBar } from '@mui/material';
+import { teal, common } from '@mui/material/colors';
+
 const theme = createTheme({
   palette: {
     mode: 'dark',
     primary: {
-      main: commonColors.white
+      main: common.white
     },
     secondary: teal,
   },
@@ -16,15 +16,18 @@ const theme = createTheme({
     MuiButton: {
       styleOverrides: {
         root: {
+          background: common.white,
+          color: common.black,
           borderRadius: '0',
           fontSize: 'large',
           margin: '0.1em',
-          paddingTop: '4em',
-          paddingBottom: '4em',
+          paddingTop: '1em',
+          paddingBottom: '1em',
           fontWeight: '800',
+          width: '10em',
           '&:hover': {
             background: '#1de9b6',
-        }
+          }
         },
       },
     },
@@ -36,7 +39,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <AppBar className="Bar" color="secondary" enableColorOnDark position="static"><h1 className="Bar-text">My instrument</h1></AppBar>
+        <AppBar className="Bar" color="secondary" enableColorOnDark position="static"><h1 className="BarText">My instrument</h1></AppBar>
         <Instrument></Instrument>
       </div>
     </ThemeProvider>
--- src/features/instrument/Instrument.tsx
+++ src/features/instrument/Instrument.tsx
@@ -1,30 +1,159 @@
 import { Button } from '@mui/material';
 import './Instrument.css';
-import { Synth, Volume, Sampler, Destination } from 'tone'
-import { GAMMA_NOTES } from '../../constants/notes'
-const CatSample = require("../../assets/cat-sample.mp3");
+import { Synth, Volume, Sampler, Destination, PolySynth, Transport, Part, Noise, AutoFilter, Player } from 'tone'
+import { BASS_CHORDS, GAMMA_NOTES, MARIO_SAMPLES, MELODY_CHORDS } from '../../constants/notes'
+const DogSample = require("../../assets/dog-sample.mp3");
+
+const keyStyles = {
+  paddingTop: '4em',
+  paddingBottom: '4em',
+  width: '5em'
+};
 
 export const Instrument = () => {
-  // @TODO: work on initializing only once (useMemo?)
-  const synth = new Synth().toDestination();
+  // @TODO: work on initializing only once (useEffect/useMemo?)
+  const mediumVolume = new Volume(-15);
 
-  const volume = new Volume(-5);
   const sampler = new Sampler({
-    G2: CatSample,
-  }).chain(volume, Destination);
+    E4: DogSample,
+  }).chain(mediumVolume, Destination);
 
-  function play(note: string) {
-    synth.triggerAttackRelease(note, "8n");
+  const polySynthSquare = new PolySynth(Synth, {
+    oscillator: {
+      type: "square",
+    },
+  }).chain(new Volume(-25), Destination);
+
+  // buuu
+  let isTransportStarted: boolean = false;
+  let melodyPart: Part | null = null;
+  let bassPart: Part | null = null;
+  let noise: Noise | null = null;
+
+  function playNote(note: string) {
+    polySynthSquare.triggerAttackRelease(note, "8n");
     sampler.triggerAttack(note);
   }
 
+  // @TODO: add types
+  function playPart(part: Part) {
+    if (!isTransportStarted) {
+      Transport.toggle();
+      isTransportStarted = true;
+      Transport.bpm.value = 132;
+    }
+
+    part.start(0);
+    part.loop = true;
+    part.loopEnd = "4m";
+  }
+
+  // @TODO: maybe move to separate components, maybe same file
+  function toggleMelody() {
+    if (melodyPart) {
+      melodyPart.mute = !melodyPart.mute;
+    } else {
+      const polySynthSaw = new PolySynth(Synth, {
+        oscillator: {
+          type: "fatsawtooth",
+        },
+        envelope: {
+          attack: 0.01,
+          release: 0.4,
+        },
+      }).chain(mediumVolume, Destination);
+
+      melodyPart = new Part((time, chord) => {
+        sampler.triggerAttackRelease(chord.note, chord.duration, time);
+        polySynthSaw.triggerAttackRelease(
+          chord.note,
+          chord.duration,
+          time
+        );
+      }, MELODY_CHORDS);
+
+      playPart(melodyPart);
+    }
+  }
+
+  function toggleBass() {
+    if (bassPart) {
+      bassPart.mute = !bassPart.mute;
+    } else {
+      bassPart = new Part((time, chord) => {
+        polySynthSquare.triggerAttackRelease(
+          chord.note,
+          chord.duration,
+          time
+        );
+      }, BASS_CHORDS);
+
+      playPart(bassPart);
+    }
+  }
+
+  function toggleNoise() {
+    if (noise) {
+      noise.mute = !noise.mute;
+    } else {
+      noise = new Noise("pink").start();
+      const filter = new AutoFilter({
+        frequency: "8m",
+      }).chain(new Volume(-20), Destination);
+
+      noise.connect(filter);
+      filter.start();
+    }
+  }
+
+  // @TODO: review isnt a beter way to write each thing now (always create new player?), also any diff in react
+  function playSample(sampleName: string) {
+    new Player({
+      url: sampleName,
+      autostart: true,
+    }).chain(mediumVolume, Destination);
+  }
+
+  function playRandomMarioSample() {
+    playSample(
+      MARIO_SAMPLES[Math.floor(Math.random() * MARIO_SAMPLES.length)]
+    );
+  }
+
+  function speedUpBpm() {
+    Transport.bpm.value += 20;
+  }
+
+  function stopInstrument(instrument: Part | Noise | null) {
+    if (instrument) {
+      instrument.mute = true;
+      instrument.stop();
+      // instrument = null;
+    }
+  }
+
+  function stopInstruments() {
+    playSample(MARIO_SAMPLES[5]);
+
+    stopInstrument(melodyPart);
+    stopInstrument(bassPart);
+    stopInstrument(noise);
+  }
 
   return (
     <div className="Instrument">
       <div>
         {GAMMA_NOTES.map(note => (
-          <Button size="large" variant="contained" color="primary" onClick={() => play(note)} key={note}>♪ {note}</Button>
+          <Button onClick={() => playNote(note)} key={note} sx={keyStyles}>♪ {note}</Button>
         ))}
+      </div>
+      <div className="PredefinedMusic">
+        <Button onClick={toggleMelody}>🎹 Melody</Button>
+        <Button onClick={toggleBass}>🎸 Bass</Button>
+        <Button onClick={toggleNoise}>💨 Noise</Button>
+        <Button onClick={playRandomMarioSample}>🍄 Mario</Button>
+        <Button onClick={speedUpBpm}>👟 Speed up</Button>
+        <Button onClick={stopInstruments}>🖐 Stop</Button>
       </div>
     </div>
 
